5eaa994ec5c84d25f7b30bcc92ca6deecaac4308
e3pojedi <46981187+e3pojedi@users.noreply.github.com>
2022-04-05 15:10:20 +1000

    Updated EmersonTestScene to create prototype example

--- KelpieGamesProject/Assets/Sandboxes/Emerson/Scripts/GlobalFlock.cs
+++ KelpieGamesProject/Assets/Sandboxes/Emerson/Scripts/GlobalFlock.cs
@@ -6,8 +6,10 @@ public class GlobalFlock : MonoBehaviour
 {
     public GameObject fishPrefab;
     public GameObject goalPrefab; //where the fish are trying to go to
-    public static int tankSize = 30; //Size of the play area (meters)
-    public static int numFish = 30; //Numb of Fish to spawn
+    public static int tankSizeX = 125; //Size of the play area (meters)
+    public static int tankSizeY = 40; //Size of the play area (meters)
+    public static int tankSizeZ = 125; //Size of the play area (meters)
+    public static int numFish = 300; //Numb of Fish to spawn
     public static GameObject[] allFish = new GameObject[numFish];
 
     public static Vector3 goalPos = Vector3.zero;
@@ -17,9 +19,9 @@ public class GlobalFlock : MonoBehaviour
     {
         for(int i=0; i< numFish; i++)
         {
-            Vector3 pos = new Vector3(Random.Range(-tankSize, tankSize),
-                Random.Range(-tankSize, tankSize),
-                Random.Range(-tankSize, tankSize));
+            Vector3 pos = new Vector3(Random.Range(-tankSizeX, tankSizeX),
+                Random.Range(-tankSizeY, tankSizeY),
+                Random.Range(-tankSizeZ, tankSizeZ));
             allFish[i] = (GameObject)Instantiate(fishPrefab, pos, Quaternion.identity);
             
         }
@@ -30,9 +32,9 @@ public class GlobalFlock : MonoBehaviour
     {
         if(Random.Range(0,100000) < 50) //this changes the target position of the fish, changes every so oftem
         {
-            goalPos = new Vector3(Random.Range(-tankSize, tankSize),
-                Random.Range(-tankSize, tankSize),
-                Random.Range(-tankSize, tankSize)); //set new goal position for fish to wander to
+            goalPos = new Vector3(Random.Range(-tankSizeX, tankSizeX),
+                Random.Range(-tankSizeY, tankSizeY),
+                Random.Range(-tankSizeZ, tankSizeZ)); //set new goal position for fish to wander to
             goalPrefab.transform.position = goalPos; //sets location of pink sphere
         }
     }
